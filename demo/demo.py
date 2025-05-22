7550fbce701ce2920e1a12b61cc7b5694260b0dc
Kespers <siracusa.speranza@gmail.com>
2025-05-22 10:25:01 +0200

    update ui

--- demo/demo.py
+++ demo/demo.py
@@ -8,55 +8,46 @@ def clear_inputs():
     return None, "", "📭 Testo cancellato o in attesa di input."
 
 custom_css = """
-#fixed-image {
+#fixed-height-image {
     height: 300px;
-    width: 100%;
-    object-fit: contain;
-}
-#fixed-textbox, #fixed-readonly {
-    height: 150px;
 }
 """
 
-with gr.Blocks(css=custom_css) as demo:
+with gr.Blocks(css = custom_css) as demo:
+
+    # Seconda riga: descrizione algoritmo
     with gr.Row():
         with gr.Column(scale=1):
-            gr.Markdown("### Cosa fa l'algoritmo?")
-            gr.Markdown("""
-            1. NOME STEP 1: \n
-                - descrizione  
-            2. NOME STEP 2 \n
-                - descrizione
-            """)
+            gr.Image(label="Steps", interactive=False, type="filepath", elem_id="fixed-height-image")
+
+
+    with gr.Row():
+        # Colonna sinistra: input immagine + pulsanti
         with gr.Column(scale=1):
-            gr.Markdown("### Carica screenshoot")
-            img_input = gr.Image(label="Carica lo screenshot", sources="upload", type="pil", elem_id="fixed-image")
+            gr.Markdown("# Carica screenshot")
+            img_input = gr.Image(label="Carica lo screenshot", sources="upload", type="pil")
             with gr.Row():
                 btn_submit = gr.Button("Submit")
                 btn_cancel = gr.Button("Cancel")
-    
-    with gr.Row():
+        
+        # Colonna destra: immagine pre-processata sopra il risultato
         with gr.Column(scale=1):
-            gr.Markdown("### Immagine pre-processata")
-            output_img = gr.Image(label="", show_label=False, elem_id="fixed-image")
-        with gr.Column(scale=1):
-            gr.Markdown("### Risultato")
-            testo_output = gr.Markdown(
-                value="📭 In attesa di input...", 
-                elem_id="fixed-readonly",
-            )
+            gr.Markdown("# Output")
+            output_img = gr.Image(label="Immagine pre-processata")
 
-    # Nuovo blocco iframe a larghezza intera
-    with gr.Row(): # Questa riga, essendo un figlio diretto di gr.Blocks, occuperà la larghezza disponibile.
+            testo_output = gr.Markdown(value="📭 In attesa di input...")
+
+    # TODO: tensorspace
+    with gr.Row():
         gr.HTML(
             """
-            <div id="iframe-container">
-                <iframe src="http://localhost:8000/tensorspace.html" width="100%" height="600" style="border:none; display: block;"></iframe>
+            <div>
+                <iframe src="http://localhost:8000/tensorspace.html" width="100%" height="600" style="border:none;"></iframe>
             </div>
             """
         )
 
-    btn_submit.click(fn=placeholder, inputs=[img_input], outputs=[output_img, testo_output])
+    btn_submit.click(fn=placeholder, inputs=[img_input, testo_output], outputs=[output_img, testo_output])
     btn_cancel.click(fn=clear_inputs, inputs=None, outputs=[img_input, testo_output])
 
 if __name__ == "__main__":
