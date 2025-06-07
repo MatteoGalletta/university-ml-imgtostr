6533910266bbd50a9ebf34d7e6138a7f64f7376b
Kespers <siracusa.speranza@gmail.com>
2025-06-07 16:36:52 +0200

    demo

--- demo/demo.py
+++ demo/demo.py
@@ -12,13 +12,15 @@ custom_css = """
 #output_text_area textarea {
     font-size: 100px;
     line-height: 1.6;
-    font-family: monospace;
 }
 
-/* Adjustments for the input image to take full width */
+textarea{
+    height: 405px !important;
+}
+
 #input_image {
-    min-height: 400px; /* Keep minimum height for consistency */
-    width: 100% !important; /* Make the input image take full width */
+    min-height: 400px;
+    width: 100% !important;
 }
 
 #input_image img {
@@ -38,20 +40,20 @@ custom_css = """
     object-fit: contain;
     border-radius: 8px;
 }
+
+footer .svelte-1rjryqp{
+    display: none !important;
+}
 """
 
 with gr.Blocks(css=custom_css) as demo:
-    # OCR Tool content directly within the block
     with gr.Row():
         with gr.Column(scale=1):
             gr.Markdown("# 1. Carica screenshot")
-            # Adjusted the img_input to take full width within its column
             img_input = gr.Image(label="Screenshot caricato", sources=["upload", "clipboard"], type="pil", elem_id="input_image", show_label=False)
             with gr.Row():
-                btn_submit = gr.Button("Submit")
-                btn_cancel = gr.Button("Cancel")
+                btn_submit = gr.Button("ELABORA")
 
-    with gr.Row():
         with gr.Column(scale=1):
             gr.Markdown("# 2. Pre-processing")
             output_img = gr.Image(label="Output immagine", elem_id="output_image", show_label=False)
@@ -63,7 +65,6 @@ with gr.Blocks(css=custom_css) as demo:
 
     # Collegamenti ai pulsanti
     btn_submit.click(fn=process_image, inputs=[img_input], outputs=[output_img, output_text])
-    btn_cancel.click(fn=clear_inputs, inputs=None, outputs=[img_input, output_img, output_text])
 
 if __name__ == "__main__":
     demo.launch()
